87f7fa7d898f536c674499b8fccc7dee2214a14c
EkaterinaBobkova <63730777+EkaterinaBobkova@users.noreply.github.com>
2020-09-02 14:24:45 +0200

    migrations and models update

---
+++ routes/publication.js
@@ -0,0 +1,33 @@
+
+
+const express = require('express'); 
+const router = express.Router(); 
+const publicationCtrl = require('../controllers/publication'); 
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config'); s
+
+
+// ROUTES //
+
+// POST //
+router.post ('/', auth, multer, publicationCtrl.createPublication); 
+
+// GET //
+router.get('/', auth, publicationCtrl.getAllPublication); 
+
+
+// GET SELECTION //
+router.get('/selection', auth, publicationCtrl.getSelection);
+
+// GET ONE //
+router.get('/:id', auth,  publicationCtrl.getOnePublication); 
+
+// PUT MODERATEUR //
+router.put('/select/:id', auth, publicationCtrl.selectPublication); 
+
+
+// EXPORT //
+
+module.exports = router;
+
+
